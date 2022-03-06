9c206ef47fd4078e3f651cceec867c0b803d9658
rphsantos1995 <rph.santos@Outlook.com>
2022-03-06 14:57:34 -0300

    App: endpoint for registering users

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
-import express from 'express';
+import express, { Router, Request, Response, NextFunction } from 'express';
+ 
+import usersService from './services/userService';
+import { IUser } from './models/interfaceUser';
+import userController from './controllers/userController';
+import { validateUser } from './middlewares/validateUser';
 
 const app = express();
 
 app.use(express.json());
+
+app.post('/user', validateUser, userController.createUser);
+
 
 export default app;
