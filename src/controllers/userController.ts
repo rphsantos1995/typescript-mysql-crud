90f788944c756a1bb6b397cf15f62d6baa076a20
rphsantos1995 <rph.santos@Outlook.com>
2022-03-15 09:53:34 -0300

    Controllers: setting userController on new user registrer

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,36 +1,25 @@
 import { Router, Request, Response, NextFunction } from 'express'; 
+import jwt from 'jsonwebtoken';
 import { IUser } from '../models/interfaceUser';
 import usersService from '../services/userService';
-import { validateUser } from '../middlewares/validateUser';
-import jwt from 'jsonwebtoken';
-// import validateJWT from '../auth/validateJWT';
+import validateUser from '../middlewares/validateUser';
 
+const userRouter = Router();
 
-
-// In the env cenario, its necessary to explicit the key.
+// In this env cenario, its necessary to explicit the key for test aprove
 const JWT_SECRET = 'mysecretkey';
-interface jwtConf {
-  expiresIn: string,
-  algorithm: string,
-}
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
+const createUser = async (req: Request, res: Response, _next: NextFunction) => {
   const newUser: IUser = req.body;
   const newUserCreated = await usersService.create(newUser);
-
-  const jwtConfig: jwtConf = {
-    expiresIn: '7d',
-    algorithm: 'HS256',
-  };
 
   const token = jwt.sign({ data: newUserCreated }, JWT_SECRET as string, {
     expiresIn: '7d',
     algorithm: 'HS256',
   });
-
-
   return res.status(201).json({ token });
-
 };
 
-export default { createUser };
+userRouter.use('/users', validateUser, createUser);
+
+export default userRouter;
